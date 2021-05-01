fae60c6b64075046f0fb67414831cf6e188eff37
Victor Holguin <vic@vics.pics>
2021-05-01 16:21:23 -0700

    added logging to useThrottle, updated lal version

--- src/useRelay/useRelay.js
+++ src/useRelay/useRelay.js
@@ -4,19 +4,20 @@ import useRequest from '../useRequest';
 import useThrottle from '../useThrottle';
 
 export default function useRelay(props) {
-	const { delay, onError = () => {}, onSuccess = () => {}, paused, mono, url, watch, ...rest  } = uno(props);
+	const { debug, delay, onError = () => {}, onSuccess = () => {}, paused, mono, url, watch, ...rest  } = uno(props);
 	const [status, setStatus] = useState(paused ? -1 : 0);
 	const execute = (res, step, onFinish) => {
 		onFinish(res);
 		setStatus(step); // 2 || 3
 	};
 	const { request, ...hooks } = useRequest({
+		debug,
 		onError: (res) => execute(res, 3, onError),
 		onSuccess: (res) => execute(res, 2, onSuccess),
 		url: mono || url,
 		...rest
 	});
-	const { watchStore, updateWatch } = useThrottle(request, status, setStatus, watch, delay);
+	const { watchStore, updateWatch } = useThrottle(request, status, setStatus, watch, delay, debug);
 
 	return {
 		request, // function to trigger API request directly
--- src/useThrottle/useThrottle.js
+++ src/useThrottle/useThrottle.js
@@ -1,17 +1,19 @@
 import { useEffect, useState } from 'react';
 
-export default function useThrottle(toDo, status, setStatus, watch, delay = 0) {
+export default function useThrottle(toDo, status, setStatus, watch, delay = 0, debug) {
 	const [watchStore, updateWatch] = useState(watch);
 
 	useEffect(() => {
 		const timer = setTimeout(() => {
 			if (!status) {
+				if (debug) console.log('hangers useThrottle switch');
 				setStatus(1);
 				toDo();
 			}
 		}, delay);
 
 		if ((watch !== watchStore) && status > 1) {
+			if (debug) console.log('hangers useThrottle watch trigger');
 			updateWatch(watch);
 			setStatus(0);
 		}
