c42321c130327cdb1b7c8c871dc1208f28b74a5d
Victor Holguin <vic@vics.pics>
2021-05-17 13:46:48 -0700

    useThrottle watch value tweak

--- src/useThrottle/useThrottle.js
+++ src/useThrottle/useThrottle.js
@@ -16,7 +16,7 @@ export default function useThrottle(toDo = () => {}, status, setStatus, watch, d
 	},[status]);
 
 	useEffect(() => {
-		if ((watch !== watchStore) && status > 1) {
+		if ((watch !== watchStore) && status !== 1) {
 			if (debug) console.log('useThrottle watch trigger');
 			updateWatch(watch);
 			setStatus(0);
