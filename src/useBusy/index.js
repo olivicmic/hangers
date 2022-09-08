f12b0cdc659276d490728c68c7c9b0cf5972d986
olivicmic <vic@vics.pics>
2022-09-07 19:17:07 -0700

    useBusy tweak

--- src/useBusy/index.js
+++ src/useBusy/index.js
@@ -4,7 +4,7 @@ export default function useBusy({ onRest = () => {}, onStart = () => {} }) {
 	const [busy, setBusy] = useState(0);
 	const [run, setRun] = useState(1);
 	return [busy, {
-		onStart: () => setBusy(run) && onStart(),
-		onRest: () => setBusy(0) && setRun(run + 1) && onRest()
+		onStart: () => setBusy(run) && setRun(run + 1) && onStart(),
+		onRest: () => setBusy(0) && onRest()
 	}, setBusy];
 };
