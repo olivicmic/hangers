71068bf67ac1a648a6b431a1ce937a0456d1835c
olivicmic <vic@vics.pics>
2022-09-07 17:40:17 -0700

    useBusy is 0 when not busy and an increasing number for each time the animation runs (1,2,3 ...) rather than simply false/true, so animations can differ on differnt runs

--- src/useBusy/index.js
+++ src/useBusy/index.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 
 export default function useBusy({ onRest = () => {}, onStart = () => {} }) {
-	const [busy, setBusy] = useState(false);
+	const [busy, setBusy] = useState(0);
+	const [run, setRun] = useState(1);
 	return [busy, {
-		onStart: () => setBusy(true) && onStart(),
-		onRest: () => setBusy(false) && onRest()
+		onStart: () => setBusy(run) && onStart(),
+		onRest: () => setBusy(0) && setRun(run + 1) && onRest()
 	}, setBusy];
 };
