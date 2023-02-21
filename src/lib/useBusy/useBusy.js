f9b380a12e5fdbff84b1b0165d664889bdc4b1b0
olivicmic <vic@vics.pics>
2023-02-21 01:02:08 -0800

    lal update, other tweaks

--- src/lib/useBusy/useBusy.js
+++ src/lib/useBusy/useBusy.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 
-export default function useBusy({ onRest = () => {}, onStart = () => {} }) {
+export default function useBusy({ disable, onRest = () => {}, onStart = () => {} }) {
 	const [busy, setBusy] = useState(false);
 	return [busy, {
-		onStart: (result, spring, item) => { setBusy(true); onStart(true, item) },
-		onRest: (result, spring, item) => { setBusy(false); onRest(false, item) }
+		onStart: (result, spring, item) => !disable && setBusy(true) && onStart(true, item),
+		onRest: (result, spring, item) => !disable && setBusy(false) && onRest(false, item)
 	}, setBusy];
 };
--- src/lib/useHover/useHover.js
+++ src/lib/useHover/useHover.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function useHover(props) {
+export default function useHover() {
 	const [ hover, hoverSet ] = useState(false);
 	return [ hover, { onMouseEnter: () => hoverSet(true), onMouseLeave: () => hoverSet(false) }];
 };
