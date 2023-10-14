e33e381a5df89f4e87b662e97032a8d765595d3c
olivicmic <vic@vics.pics>
2023-10-14 16:37:15 -0700

    revised useHover

--- src/lib/useHover/useHover.js
+++ src/lib/useHover/useHover.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
-export default function useHover() {
+export default function useHover({ onMouseEnter = () => {}, onMouseLeave = () => {} }) {
 	const [ hover, hoverSet ] = useState(false);
-	return [ hover, { onMouseEnter: () => hoverSet(true), onMouseLeave: () => hoverSet(false) }];
+	const onHover = x => () => { hoverSet(x); x ? onMouseEnter() : onMouseLeave() };
+	return [ hover, { onMouseEnter: onHover(true), onMouseLeave: onHover(false)}];
 };
